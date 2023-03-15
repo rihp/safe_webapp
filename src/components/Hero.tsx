4f3b75d0403d1cbce1fba58eb9fc39fd5f5d745f
DaoAdvocate <12145726+rihp@users.noreply.github.com>
2023-03-15 11:52:12 +0100

    testing_workflows

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,6 +6,8 @@ function Hero() {
       <h1>Safe Webapp</h1>
       <p>This is a demo app to showcase the functionality of the Safe wrapper</p>
       <p>Follow along to see a step by step of the creation of a safe and its features</p>
+      <p>Polywrap is a WebAssembly (WASM) runtime that enables developers to write code in any language and compile it to WASM. This allows developers to create applications that can run on any platform, including web browsers, mobile devices, and servers. Polywrap provides a secure and efficient way to run code on multiple platforms, as well as a way to easily port existing code to WASM. It also provides a number of features that make it easier to develop and debug applications, such as a virtual machine, a debugger, and a compiler. Additionally, Polywrap provides a number of tools to help developers create and deploy applications quickly and easily. These tools include a package manager, a build system, and a deployment system. With Polywrap, developers can create applications that are secure, efficient, and easy to deploy.</p>
+
     </div>
   );
 }
